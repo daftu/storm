6a6478dd74762f4b54d30e2171b398fbfbc4c963
daftu <dm@rudasl.pl>
2025-06-23 21:43:05 +0200

    Require paramiko>=3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         ],
     },
     install_requires=list(filter(None, [
-        "paramiko",
+        "paramiko>=3",
         "termcolor",
         "Flask>=2.0",
         "argparse" if sys.version_info[:2] < (2, 7) else None,
